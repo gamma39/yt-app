207d547ce7fa289ab805854facc590e15ce4d693
gamma39 <pugsyblack@hotmail.com>
2021-05-15 18:01:46 -0700

    changed searchBar label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ const SearchBar = ({ onFormSubmit }) => {
         <div className="search-bar ui segment">
             <form onSubmit={onSubmit} className="ui form">
                 <div className="field">
-                    <label htmlFor="">Search</label>
+                    <label htmlFor="">Video Search</label>
                     <input 
                         type="text"
                         value={term}
